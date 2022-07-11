1a02654c96e25f88be20d0393bd1dd3ceb12f336
VZ857 <zayac566@mail.ru>
2022-07-11 16:51:51 +0700

    Задача № 1

---
+++ hello python/zadacha1.py
@@ -0,0 +1,11 @@
+
+
+
+print ("Введите число: ")
+Number = int(input()) 
+if 6 <= Number <= 7:
+        print("Выходной день")
+elif 0 < Number < 6:
+        print("Будний день")
+else:
+        print("Введите число от 1 до 7")
