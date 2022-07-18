dd4468ed8160c8b7d9e8db511315859474d01ef8
VZ857 <zayac566@mail.ru>
2022-07-18 17:11:19 +0700

    Задача #2 Практика 3

---
+++ hello python/Yrok3 dz2.py
@@ -0,0 +1,15 @@
+
+
+    # Напишите программу, которая найдёт произведение пар чисел списка. 
+    # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+
+def pro(lst):
+    n = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+    lst = [lst[i]*lst[len(lst)-i-1] for i in range(n)]
+    print(lst)
+
+        
+lst = list(map(int, input("Введите числа через запятую: ").split(',')))
+pro(lst)
+
