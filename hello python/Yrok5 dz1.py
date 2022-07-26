f098f0259afa680874dfc0eb29bfa833725196df
VZ857 <zayac566@mail.ru>
2022-07-26 11:34:12 +0700

    Задача #1 Практика 5

--- hello python/Yrok5 dz1.py
+++ hello python/Yrok5 dz1.py
@@ -1,6 +1,25 @@
 
+# Удаление абв
+# str = 'Привет абв мир ,я любабвлю питонабв'
+# print ("Исходная строка: " + str) 
+# res_str = str.replace('абв', '') 
+# print("Строка после удаления всех символов абв: " + res_str) 
+
+# Удаление через for
+# str = 'Привет абв мир ,я любабвлю питонабв'
+# print(f"Исходный текст: {str}")
+# find_txt = "абв"
+# lst = [i for i in str.split() if find_txt not in i]
+# print(f'Результат: {" ".join(lst)}')
+
+
+# Удаление с помощью filter
 
 str = 'Привет абв мир ,я любабвлю питонабв'
-print ("Исходная строка: " + str) 
-res_str = str.replace('абв', '') 
-print("Строка после удаления всех символов абв: " + res_str) 
+def delet(str):
+    str = list(filter(lambda i: 'абв' not in i, str.split()))
+    return " ".join(str)
+
+str = delet(str)
+
+print(str)
