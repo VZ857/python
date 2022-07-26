64dae3711debe245f283c513ecb4851427f09d19
VZ857 <zayac566@mail.ru>
2022-07-26 12:46:05 +0700

    Практика 6

--- hello python/Yrok5 dz1.py
+++ hello python/Yrok5 dz1.py
@@ -18,7 +18,7 @@
 str = 'Привет абв мир ,я любабвлю питонабв'
 def delet(str):
     str = list(filter(lambda i: 'абв' not in i, str.split()))
-    return " ".join(str)
+    return ''.join(str)
 
 str = delet(str)
 
